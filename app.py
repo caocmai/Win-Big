21f07d93679441ffe29aaa6cf732d9485a0d9ca9
Cao Mai <caomai@rocketmail.com>
2019-10-15 21:12:37 -0700

    Setting up environment

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
-wi_n = [2,3,6,9]
-# gu_n = [2,3,7,9]
-gu_n = [[2,3,4,5], [2,3,6,8], [2,3,6,9], [5,6,7,8]]
+import random
+random.seed(40)
+from flask import Flask
 
-# def win(win_nums, guessed_nums):
-#   for i in range(len(guessed_nums)):
-#     for numbers in guessed_nums[i]:
-#       if numbers not in win_nums:
-#         return False
-#     return True
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+winning_nums = [2,3,6]
+# all_guessed_nums = [2,3,7,9]
+all_guessed_nums = [[2,3,4,5], [2,3,6], [2,3,5], [5,6,7,8]]
+
+# random_guessed_nums = []
+
+# def generate_random_num(times):
+#   for _ in range(times):
+#     random_num = random.randrange(1,11)
+#     random_guessed_nums.append(random_num)
+
+# generate_random_num(3)
+# print(random_guessed_nums)
+
 
 def win(win_nums, guessed_nums):
-  for num in guessed_nums:
-    if num not in win_nums:
-      return False
+  for i in range(len(win_nums)):
+      if win_nums[i] != guessed_nums[i]:
+          return False
   return True
 
-# print(win(wi_n, gu_n))
+# print(win(winning_nums, all_guessed_nums))
 
 def check_all(win_nums, guessed_nums_list):
+  win_count = 0
   for i in range(len(guessed_nums_list)):
     if win(win_nums, guessed_nums_list[i]):
-      print("you win")
-    else:
-      print("you lose")
+      win_count += 1
+    # else:
+      # print("you lose")
+  return win_count
 
-check_all(wi_n, gu_n)
+print(f"You won {check_all(winning_nums, all_guessed_nums)} out of {len(all_guessed_nums)} plays.")
+print("For " + str(check_all(winning_nums, all_guessed_nums)/len(all_guessed_nums)) + " percent.")
+# print((check_all(winning_nums, all_guessed_nums)/len(all_guessed_nums)))
---
+++ helper.py
@@ -0,0 +1,48 @@
+import random
+# random.seed(40)
+
+winning_nums = [] # 8,10,9
+guessed_nums = []
+
+for _ in range(3):
+  random_num = random.randrange(1,11)
+  winning_nums.append(random_num)
+
+def win(win_nums, guessed_nums):
+    for i in range(len(win_nums)):
+        if win_nums[i] != guessed_nums[i]:
+            return False
+    return True
+
+count_lose = 0
+count_win = 0
+# running = True
+while count_win <= 0:
+  # random.seed(30) # 9,5,10
+  guessed_nums.sort()
+  winning_nums.sort()
+
+  for _ in range(3):
+    random_num = random.randrange(1,11)
+    guessed_nums.append(random_num)
+  # print(guessed_nums)
+  
+  if win(winning_nums,guessed_nums):
+    count_win += 1
+    print(f"Guessed number: {guessed_nums}")
+    print(f"Winning Number: {winning_nums}")
+
+    print("win")
+    # break
+  else:
+    count_lose += 1
+    # print("lose")
+
+  guessed_nums = []
+  
+  
+print(f"win count: {count_win}")
+print(f"lose count: {count_lose}")
+
+print("probablity of wiining " + str(round((count_win/count_lose * 100), 2 )))
+print(f"Winning numbers are: {winning_nums}")
